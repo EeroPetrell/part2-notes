88b896be37d2c47153a0c1029c192e8c095d421e
Eero Petrell <eero.petrell@aalto.fi>
2020-07-21 09:28:39 +0300

    osa2 vikat muutokset

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
+import noteService from './services/notes'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('') 
   const [showAll, setShowAll] = useState(true)
 
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+  
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
@@ -15,13 +24,28 @@ const App = (props) => {
       id: notes.length + 1,
     }
   
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    noteService 
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
   }
 
   const handleNoteChange = (event) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
+  }
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, importnat: !note.important }
+    
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
   }
 
   const notesToShow = showAll
@@ -38,7 +62,11 @@ const App = (props) => {
       </div>      
       <ul>
         {notesToShow.map((note, i) => 
-          <Note key={i} note={note} />
+          <Note 
+            key={i} 
+            note={note} 
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         )}
       </ul>
       <form onSubmit={addNote}>
